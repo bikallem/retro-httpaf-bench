bb42bb92239cb8fb808bb2fb1845739a411955a6
Bikal Lem <gbikal+git@gmail.com>
2022-02-15 17:13:28 +0000

    cohttp-eio: update vendors

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xe
 
-run_duration="${RUN_DURATION:-30}"
+run_duration="${RUN_DURATION:-60}"
 
 # export GOMAXPROCS=1
 export COHTTP_DOMAINS=48
@@ -11,8 +11,8 @@ mkdir -p output
 
 for cmd in "cohttp_eio_parser_angstrom.exe" "cohttp_eio_parser_http.exe" "httpaf_eio.exe" "rust_hyper.exe" "cohttp_lwt_unix.exe" "httpaf_lwt.exe" "httpaf_effects.exe" "nethttp_go.exe" "httpaf_shuttle_async.exe"; do
 # for cmd in "httpaf_eio.exe" "httpaf_effects.exe" "httpaf_shuttle_async.exe" "httpaf_lwt.exe" "cohttp_eio.exe" "cohttp_lwt_unix.exe"; do
-  # for rps in 1000 50000 75000 150000 300000 400000; do
-  for rps in 150000 300000 400000; do
+  for rps in 1000 50000 75000 150000 300000 400000; do
+  # for rps in 150000 300000 400000; do
     for cons in 1000; do
       ./build/$cmd &
       running_pid=$!
