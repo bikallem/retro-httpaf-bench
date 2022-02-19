ce0f13be4a1a204fe84655e4026fb3259db11028
Bikal Lem <gbikal+git@gmail.com>
2022-02-19 12:09:09 +0000

    update latest ocaml-cohttp

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -14,19 +14,19 @@ mkdir -p output
 # for cmd in "cohttp_eio_parser_http.exe" "httpaf_eio.exe" "rust_hyper.exe" "cohttp_lwt_unix.exe" "httpaf_lwt.exe" "httpaf_effects.exe" "nethttp_go.exe" "httpaf_shuttle_async.exe"; do
 # for cmd in "cohttp_eio_parser_angstrom.exe" "cohttp_eio_parser_http.exe" "httpaf_eio.exe" "rust_hyper.exe" "cohttp_lwt_unix.exe" "httpaf_lwt.exe" "httpaf_effects.exe" "nethttp_go.exe" "httpaf_shuttle_async.exe"; do
 # for cmd in "httpaf_eio.exe" "httpaf_effects.exe" "httpaf_shuttle_async.exe" "httpaf_lwt.exe" "cohttp_eio.exe" "cohttp_lwt_unix.exe"; do
-# for cmd in "httpaf_eio.exe" "cohttp_eio_parser_http.exe" "cohttp_eio_parser_angstrom.exe" ; do
+for cmd in "httpaf_eio.exe" "cohttp_eio_parser_http.exe" "cohttp_eio_parser_angstrom.exe" ; do
 # for cmd in "cohttp_eio_parser_http.exe"; do
 # for cmd in "cohttp_eio_parser_custom.exe" ; do
-for cmd in "httpaf_eio.exe" ; do
-  # for rps in 300000 400000 800000 1500000; do
-  for rps in 1500000; do
+# for cmd in "httpaf_eio.exe" ; do
+  for rps in 300000 400000 800000 1500000; do
+  # for rps in 1500000; do
     for cons in 1000; do
       ./build/$cmd &
       running_pid=$!
       sleep 2;
       ./build/wrk2 -t 24 -d${run_duration}s -L -s ./build/json.lua -R $rps -c $cons http://localhost:8080 > output/run-$cmd-$rps-$cons.txt;
-       curl http://localhost:8080/exit
-      # kill ${running_pid};
+       # curl http://localhost:8080/exit
+      kill ${running_pid};
       sleep 1;
     done
   done
