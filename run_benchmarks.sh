3a91a10e3061d0debd46f004cfe0710a8fd166b9
Bikal Lem <gbikal+git@gmail.com>
2022-02-22 22:51:25 +0000

    update latest ocaml-cohttp

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xe
 
-run_duration="${RUN_DURATION:-30}"
+run_duration="${RUN_DURATION:-60}"
 
 export GOMAXPROCS=1
 export COHTTP_DOMAINS=24
@@ -14,19 +14,19 @@ mkdir -p output
 # for cmd in "cohttp_eio_parser_http.exe" "httpaf_eio.exe" "rust_hyper.exe" "cohttp_lwt_unix.exe" "httpaf_lwt.exe" "httpaf_effects.exe" "nethttp_go.exe" "httpaf_shuttle_async.exe"; do
 # for cmd in "cohttp_eio_parser_angstrom.exe" "cohttp_eio_parser_http.exe" "httpaf_eio.exe" "rust_hyper.exe" "cohttp_lwt_unix.exe" "httpaf_lwt.exe" "httpaf_effects.exe" "nethttp_go.exe" "httpaf_shuttle_async.exe"; do
 # for cmd in "httpaf_eio.exe" "httpaf_effects.exe" "httpaf_shuttle_async.exe" "httpaf_lwt.exe" "cohttp_eio.exe" "cohttp_lwt_unix.exe"; do
-# for cmd in "httpaf_eio.exe" "cohttp_eio_parser_http.exe" "cohttp_eio_parser_custom.exe" ; do
+for cmd in "httpaf_eio.exe" "cohttp_eio_parser_http.exe" "cohttp_eio_parser_custom.exe" ; do
 # for cmd in "cohttp_eio_parser_http.exe"; do
-for cmd in "cohttp_eio_parser_custom.exe" ; do
+# for cmd in "cohttp_eio_parser_custom.exe" ; do
 # for cmd in "httpaf_eio.exe" ; do
-  # for rps in 400000 800000 1500000; do
-  for rps in 1500000; do
+  for rps in 400000 800000 1500000; do
+  # for rps in 1500000; do
     for cons in 1000; do
       ./build/$cmd &
       running_pid=$!
       sleep 2;
       ./build/wrk2 -t 24 -d${run_duration}s -L -s ./build/json.lua -R $rps -c $cons http://localhost:8080 > output/run-$cmd-$rps-$cons.txt;
-      curl http://localhost:8080/exit
-      # kill ${running_pid};
+      # curl http://localhost:8080/exit
+      kill ${running_pid};
       sleep 1;
     done
   done
